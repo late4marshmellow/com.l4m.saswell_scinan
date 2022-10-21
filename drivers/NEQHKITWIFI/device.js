849f5b38223378b5a0d5b574d6f7408c4acf7243
late4marshmellow <79985161+late4marshmellow@users.noreply.github.com>
2022-10-21 23:19:44 +0200

    Add files via upload

---
+++ drivers/NEQHKITWIFI/device.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const ScinanDevice = require('../../lib/ScinanDevice2.js');
+
+class NEQHKITWIFIDevice extends ScinanDevice {}
+
+module.exports = NEQHKITWIFIDevice;
---
+++ drivers/NEQHKITWIFI/driver.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const ScinanDriver = require('../../lib/ScinanDriver2.js');
+
+class NEQHKITWIFIDriver extends ScinanDriver {}
+
+module.exports = NEQHKITWIFIDriver;
