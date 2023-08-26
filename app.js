82e3968fe51b33dce274aff63d65cdb0f2caa33c
late4marshmellow <79985161+late4marshmellow@users.noreply.github.com>
2023-08-26 02:59:05 +0200

    major fixes
    authentications should work
    adding devices should work
    still need to look into device control

--- app.js
+++
@@ -1,9 +0,0 @@
-const Homey = require('homey');
-
-class ScinanApp extends Homey.App {
-  async onInit() {
-    this.log('Successfully init Scinan version:', Homey.manifest.version);
-  }
-}
-
-module.exports = ScinanApp;
